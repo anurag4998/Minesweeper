6649624e9f9a1f1de3cafe9b717cd73e6e0c8262
Anurag <anuragagrawal4998@gmail.com>
2021-03-05 10:55:57 +0530

    update app.js mine number update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
  
   return (
     <div className="App">
-        <Structure rows = {14} columns = {10} mines = {10}/>
+        <Structure rows = {14} columns = {10} mines = {14}/>
     </div>
   );
 }
